4f23a812b23c975e37169a1ba1152cdef18a477f
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2024-12-27 16:15:23 +0530

    complete day2 and solve 2 medium and 1 hard problem

--- src/array/Leetcode15.java
+++ src/array/Leetcode15.java
@@ -2,25 +2,18 @@ package array;
 import java.util.*;
 
 public class Leetcode15 {
-
-
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-
         if (n < 3) {
             return ans;
         }
-
         Arrays.sort(nums);
-
-
         for (int i = 0; i < n; i++) {
             // avoiding duplicate as well
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-
 
             int fixValue = nums[i];
             int target = -fixValue;
@@ -38,7 +31,6 @@ public class Leetcode15 {
                     while (start < end && nums[start] == nums[start + 1]) start++;
                     while (start < end && nums[end] == nums[end - 1]) end--;
 
-
                     List<Integer> st = new ArrayList<>();
                     st.add(fixValue);
                     st.add(nums[start]);
@@ -47,7 +39,6 @@ public class Leetcode15 {
                     ans.add(st);
                     start++;
                     end--;
-
                 }
             }
         }
